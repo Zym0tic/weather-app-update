f017ce0acf2fa98abc628716828b62d9f806d16c
zym0tic <lukerozon@gmail.com>
2023-01-06 15:10:42 -0500

    create time and date component

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -6,15 +6,27 @@ function Inputs() {
     <div className="flex flex-row justify-center my-6">
       <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
         <input
-         type="text" placeholder="search for city..." className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase" 
-         />
-         <UilSearch size={25} className="text-white cursor-pointer transition ease-out hover:scale-125"/ >
-         <UilLocationPoint size={25} className="text-white cursor-pointer transition ease-out hover:scale-125"/ >
+          type="text"
+          placeholder="search for city..."
+          className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase"
+        />
+        <UilSearch
+          size={25}
+          className="text-white cursor-pointer transition ease-out hover:scale-125"
+        />
+        <UilLocationPoint
+          size={25}
+          className="text-white cursor-pointer transition ease-out hover:scale-125"
+        />
       </div>
       <div className="flex flex-row w-1/4 items-center justify-center">
-        <button name="metric" className="text-xl text-white font-light">°C</button>
+        <button name="metric" className="text-xl text-white font-light">
+          °C
+        </button>
         <p className="text-xl text-white mx-1">|</p>
-        <button name="imperial" className="text-xl text-white font-light">°F</button>
+        <button name="imperial" className="text-xl text-white font-light">
+          °F
+        </button>
       </div>
     </div>
   );
